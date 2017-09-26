a55b54010369c64b472f581475f50804f15f687f
mixelpixel <mixelpix@mac.com>
2017-09-25 22:04:49 -0400

    read file in, decompose to array of binary cmds

--- microbusarchitecture.js
+++ microbusarchitecture.js
@@ -1,15 +1,25 @@
-"use strict";
-
+'use strict';
+/* eslint no-console: 0 */
 const fs = require('fs');
 const os = require('os');
+// load all at once or read line by line; ignore pound sign start and comments, convert integer
+// const text = fs.readFileSync('./inputfile', 'utf8');
+// const commands = text.split(os.EOL);
+
+// const commands = fs.readFileSync('./inputfile', 'utf8').split(os.EOL);
+const process = require('process'); // <~~~ just making linter happy
+const file = process.argv[2];
+const commands = fs.readFileSync(file, 'utf8').split(os.EOL);
+const binaryCmds = [];
+commands.forEach((x) => {
+  binaryCmds.push(x.slice(0, 7));
+});
+binaryCmds.pop();
+
+console.log(binaryCmds);
 
 const MAR = [];
-const text = fs.readFileSync('./inputfile', 'utf8'); // load all at once or read line by line; ignore pound sign start and comments, convert integer
-const commands = text.split(os.EOL)
-
-console.log(commands);
-
-// let programCounter = 0
+let programCounter = 0
 
 // create interval with setInterval
 // use console logs to observer what happens
@@ -26,18 +36,3 @@ console.log(commands);
 
 
 // save next is the number 4 and it is storing 8
-
-
-
-// const fs = require('fs')
-// const readline = require('readline')
-//
-// fs.readFile('./input.txt', 'utf8', (err, txt) => {
-//   if (err) console.log(err)
-//   // console.log(txt)
-//   const instream = fs.createReadStream('input.txt')
-//   const rl = readline.createInterface(instream)
-//   rl.on('line', (line) => {
-//     console.log(line + '!!');
-//   })
-// })
