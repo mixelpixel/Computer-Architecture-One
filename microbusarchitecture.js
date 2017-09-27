0062505b2e1daa3e4b6213829acaf922ef25f178
Colombianito66 <colombianito66@gmail.com>
2017-09-26 19:38:24 -0700

    read-in command list and create instruction array

--- microbusarchitecture.js
+++ microbusarchitecture.js
@@ -1,4 +1,12 @@
 // Read Command List >> Into Array
+const fs = require('fs');
+const os = require('os');
+
+const instructions = fs.readFileSync('inputFile', 'utf8').split(os.EOL);
+const bins = []; 
+instructions.forEach((item)=> { bins.push(item.slice(0, 8))});
+
+console.log(bins);
 
 // STATE CHANGES For State Machine
   // SET 
@@ -10,7 +18,7 @@
   // MEMORY_ADDRESS_REGISTER
   // ACTIVE_REGISTER
   // MULTIPLY_REGISTER
-  
+
 // FUNCTIONS
   // EXECUTE_CPU_CYCLE
       // Program counter++ advances with each execution of the CPU cycle
