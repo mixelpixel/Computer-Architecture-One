0452171b6eea5b27c05e5472d9c8e35db248df27
Colombianito66 <colombianito66@gmail.com>
2017-09-26 18:32:22 -0700

    broad strokes

---
+++ microbusarchitecture.js
@@ -0,0 +1,23 @@
+// Read Command List >> Into Array
+
+
+
+// STATE CHANGES For State Machine
+  // SET 
+
+  // SAVE
+
+  // MULTIPLY
+
+// FUNCTIONS
+  // BINARY_STRING TO DECIMAL CONVERSION: parseInt(string, 2)
+
+  // INIT
+
+  // SET 
+
+  // SAVE
+
+  // MULTIPLY
+
+  // PRINT 
