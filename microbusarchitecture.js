4d7aace4a22a97b996b57c01130ce448148e02a7
Colombianito66 <colombianito66@gmail.com>
2017-09-26 18:41:02 -0700

    broad strokes

--- microbusarchitecture.js
+++ microbusarchitecture.js
@@ -1,28 +1,17 @@
 // Read Command List >> Into Array
 
-
-
 // STATE CHANGES For State Machine
   // INITIALIZE PROGRAM COUNTER: 0
-
   // SET 
-
   // SAVE
-
   // MULTIPLY
 
 // FUNCTIONS
-  // BINARY_STRING TO DECIMAL CONVERSION: parseInt(string, 2)
-
   // EXECUTE_CPU_CYCLE
       // Program counter++ advances with each execution of the CPU cycle
-
+  // BINARY_STRING TO DECIMAL CONVERSION: parseInt(string, 2)
   // INIT
-
   // SET 
-
   // SAVE
-
   // MULTIPLY
-
   // PRINT 
