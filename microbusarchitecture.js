bca6c1fce072a545d14fc303970d56d5df15502a
Colombianito66 <colombianito66@gmail.com>
2017-09-26 18:52:58 -0700

    broad strokes

--- microbusarchitecture.js
+++ microbusarchitecture.js
@@ -1,11 +1,16 @@
 // Read Command List >> Into Array
 
 // STATE CHANGES For State Machine
-  // INITIALIZE PROGRAM COUNTER: 0
   // SET 
   // SAVE
   // MULTIPLY
 
+// REGISTRIES
+  // INITIALIZE PROGRAM COUNTER: 0
+  // MEMORY_ADDRESS_REGISTER
+  // ACTIVE_REGISTER
+  // MULTIPLY_REGISTER
+  
 // FUNCTIONS
   // EXECUTE_CPU_CYCLE
       // Program counter++ advances with each execution of the CPU cycle
